ba8f2a59c8174e919d6b8e7ff0172584939d8bc3
jannunzi <jannunzi@gmail.com>
2018-06-04 17:37:08 -0400

    got started with course navigator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import {FormsModule} from "@angular/forms";
+import {CourseNavigatorServiceClient} from "./services/course-navigator.service.client";
+import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    CourseNavigatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    CourseNavigatorServiceClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
