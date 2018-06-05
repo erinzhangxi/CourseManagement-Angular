b67327ca0aaaa0c2b58b61aad1c7c698257a360f
jannunzi <jannunzi@gmail.com>
2018-06-05 18:25:54 -0400

    lecture 6/5/2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,37 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
 import {FormsModule} from "@angular/forms";
 import {CourseNavigatorServiceClient} from "./services/course-navigator.service.client";
 import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
+import { WhiteBoardComponent } from './white-board/white-board.component';
+import { CourseGridComponent } from './course-grid/course-grid.component';
+import {CourseServiceClient} from "./services/course.service.client";
+import {routing} from "./app.routing";
+import { CourseViewerComponent } from './course-viewer/course-viewer.component';
+import { ModuleListComponent } from './module-list/module-list.component';
+import {ModuleServiceClient} from "./services/module.service.client";
+import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import {LessonServiceClient} from "./services/lesson.service.client";
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
-    CourseNavigatorComponent
+    CourseNavigatorComponent,
+    WhiteBoardComponent,
+    CourseGridComponent,
+    CourseViewerComponent,
+    ModuleListComponent,
+    LessonTabsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   providers: [
-    CourseNavigatorServiceClient
+    CourseNavigatorServiceClient,
+    CourseServiceClient,
+    ModuleServiceClient,
+    LessonServiceClient
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { Routes, RouterModule }
+  from '@angular/router';
+import { WhiteBoardComponent }
+  from "./white-board/white-board.component";
+import {CourseViewerComponent} from "./course-viewer/course-viewer.component";
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: WhiteBoardComponent},
+  { path: 'course/:courseId', component: CourseViewerComponent },
+  { path: 'course/:courseId/module/:moduleId', component: CourseViewerComponent },
+  { path: 'course/:courseId/module/:moduleId/lesson/:lessonId', component: CourseViewerComponent },
+  { path: '**', component: WhiteBoardComponent} // last
+];
+export const routing = RouterModule.forRoot(appRoutes);
